d8a3e0ab975efa82843170554cba152f27f4d166
Warren Pearson <warren.pearson@gmail.com>
2024-11-17 16:27:41 +0000

    initial scripts

---
+++ brew_installs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# brew install --cask 1password
+# brew install --cask alfred
+# brew install --cask appcleaner
+# brew install --cask audacity
+# brew install --cask deckset
+# brew install --cask docker
+# brew install --cask firefox
+# brew install --cask rectangle-pro
+# brew install --cask piezo
+# brew install --cask scapple
+# brew install --cask signal
+# brew install --cask tableplus
+# brew install --cask transmission
+# brew install --cask transmit
+# brew install --cask vivaldi
+# brew install --cask vlc
+# brew install --cask zoom
+
+# brew install ack
+# brew install ansible
+# brew install tree
+# brew install wget
+# brew install bash
+# brew install pyenv
